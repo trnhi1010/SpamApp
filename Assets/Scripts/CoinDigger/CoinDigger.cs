34c6e053a7f881b0f03a702699c65d17c79131ee
trnhi1010 <trnhi1010@gmail.com>
2021-11-08 20:26:35 +0700

    Nhi

--- Assets/Scripts/CoinDigger/CoinDigger.cs
+++ Assets/Scripts/CoinDigger/CoinDigger.cs
@@ -9,19 +9,23 @@ public class CoinDigger : MonoBehaviour
     public Text txt_Score;
     public Text txt_Warning;
 
-    public Animator animClick;
+    public Image img_Guard;
+    public Image img_busted;
+
+    //public Animator animClick;
     int score;
 
-    public List<GameObject> time;
-    public bool isNight = false;
+    public List<GameObject> timeList;
+    public bool isNight;
 
     private void Start()
     {
-        
+        SwitchTime();
+        InvokeRepeating("SwitchTime", 2f, 2f);
+
     }
     private void Update()
     {
-        
     }
 
     public void ButtonGoldOnClick()
@@ -35,7 +39,30 @@ public class CoinDigger : MonoBehaviour
         {
             txt_Warning.gameObject.SetActive(true);
             txt_Warning.text = "You are so busted";
+            img_busted.gameObject.SetActive(true);
+            img_Guard.gameObject.SetActive(false);
             Time.timeScale = 0f;
+        }
+    }
+
+    void SwitchTime()
+    {
+        int n = Random.Range(0, 2);
+        Debug.Log(n);
+        switch(n)
+        {
+            case 0:
+                timeList[0].SetActive(true);
+                timeList[1].SetActive(false);
+                img_Guard.gameObject.SetActive(true);
+                isNight = false;
+                break;
+            case 1:
+                timeList[0].SetActive(false);
+                timeList[1].SetActive(true);
+                img_Guard.gameObject.SetActive(false);
+                isNight = true;
+                break;
         }
     }
 }
